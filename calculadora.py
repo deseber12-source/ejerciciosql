a0d5150ff51b41881c47d4c40ae89c3cd7c76e3d
Eduardo Ismael <deseber12@gmail.com>
2026-07-07 22:21:50 -0600

    add calculadora,py

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Calculadora básica")
+    print(f"Suma: 5 + 3 = {suma(5, 3)}")
+    print(f"Resta: 10 - 4 = {resta(10, 4)}")
